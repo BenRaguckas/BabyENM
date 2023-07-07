3391833d7cd71ee605efdc4b555e656e6037786d
Benas Raguckas <benas.raguckas@ericsson.com>
2023-07-07 10:13:07 +0100

    Initial commit

---
+++ src/main/java/com/goTeam/BabyENM/BabyEnmApplication.java
@@ -0,0 +1,13 @@
+package com.goTeam.BabyENM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyEnmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyEnmApplication.class, args);
+	}
+
+}
