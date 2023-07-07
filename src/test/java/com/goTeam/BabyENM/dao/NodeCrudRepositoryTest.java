3221e452d277a6b0dbb6a36505808032000fc0ce
eragben <BenRaguckas@Gamil.com>
2023-07-07 13:48:32 +0100

    Crud changes.

---
+++ src/test/java/com/goTeam/BabyENM/dao/NodeCrudRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.goTeam.BabyENM.dao;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeCrudRepositoryTest {
+
+    //@SpringBootTest
+    public void test(){}
+}
